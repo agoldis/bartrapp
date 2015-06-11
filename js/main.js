4e7154c9962d08020f1eae534eaaca0a9a434c80
Andrew Goldis <agoldis@rollout.io>
2015-06-12 04:34:51 +1000

    moved one level up

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready( function () {
+
+  // init background slideshow
+  $('#welcome-section').backstretch([
+        "img/bg/001.jpg",
+        "img/bg/002.jpg",
+        "img/bg/003.jpg",
+        "img/bg/004.jpg",
+        "img/bg/005.jpg",
+        "img/bg/006.jpg",
+    ], {duration: 3000, fade: 750});
+});
